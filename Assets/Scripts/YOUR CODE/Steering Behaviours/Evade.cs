f024a4e5358d8c1e946cf83ddbb5c6ca3c65d676
Medjed571 <danielcork8@gmail.com>
2025-11-30 18:11:10 +0000

    Evade Behaviour

---
+++ Assets/Scripts/YOUR CODE/Steering Behaviours/Evade.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Evade : SteeringBehaviour
+{
+	public override Vector3 UpdateBehaviour(SteeringAgent steeringAgent)
+	{
+		var nearestEnemy = SteeringAgent.GetNearestAgent(transform.position, GameData.Instance.enemies); ;
+
+		Vector3 targetPosition = nearestEnemy.transform.position + nearestEnemy.CurrentVelocity;
+		targetPosition.z = 0.0f;
+
+		// Get the desired velocity for seek and limit to maxSpeed
+		desiredVelocity = Vector3.Normalize(targetPosition - transform.position) * -SteeringAgent.MaxCurrentSpeed;
+
+		// Calculate steering velocity
+		steeringVelocity = desiredVelocity - steeringAgent.CurrentVelocity;
+		Debug.Log(steeringVelocity);
+		return steeringVelocity;
+	}
+}
